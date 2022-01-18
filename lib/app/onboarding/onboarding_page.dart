fc0471f9eb0e221bb666ab56c8f1414cbfccf4a6
Danial Othman <danialbinothman@gmail.com>
2022-01-18 09:57:49 +0800

    Update onboardingpage and gitignore

--- lib/app/onboarding/onboarding_page.dart
+++ lib/app/onboarding/onboarding_page.dart
@@ -13,35 +13,39 @@ class OnboardingPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Track your time.\nBecause time counts.',
-              style: Theme.of(context).textTheme.headline4,
-              textAlign: TextAlign.center,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Track your time.\nBecause time counts.',
+                  style: Theme.of(context).textTheme.headline4,
+                  textAlign: TextAlign.center,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.4,
+                  child: SvgPicture.asset('assets/time-tracking.svg',
+                      semanticsLabel: 'Time tracking logo'),
+                ),
+                CustomRaisedButton(
+                  onPressed: () => onGetStarted(context, ref),
+                  color: Colors.indigo,
+                  borderRadius: 30,
+                  child: Text(
+                    'Get Started',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5!
+                        .copyWith(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-            FractionallySizedBox(
-              widthFactor: 0.5,
-              child: SvgPicture.asset('assets/time-tracking.svg',
-                  semanticsLabel: 'Time tracking logo'),
-            ),
-            CustomRaisedButton(
-              onPressed: () => onGetStarted(context, ref),
-              color: Colors.indigo,
-              borderRadius: 30,
-              child: Text(
-                'Get Started',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline5!
-                    .copyWith(color: Colors.white),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
